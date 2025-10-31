2abf16c81d7ac4a4e90ca0e52b8daa22e6e5d334
irinaboiko <irinaboiko95@gmail.com>
2025-10-31 16:39:59 +0300

    add homepage

---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+
+import { events, IEvent } from "@/lib/constants";
+
+const Page = () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev
+        <br />
+        You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events.map((event: IEvent) => (
+            <li key={event.title}>
+              <EventCard event={event} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+export default Page;
